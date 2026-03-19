a72733909a2783d99c57fb81359f2f37ac29a7a6
whitecity01 <kh081104@gmail.com>
2026-03-20 01:41:10 +0900

    feat: 역 정보 DAO 구현

---
+++ src/main/java/com/timespot/backend/domain/place/dao/StationRepository.java
@@ -0,0 +1,18 @@
+package com.timespot.backend.domain.place.dao;
+
+import com.timespot.backend.domain.place.model.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * PackageName : com.timespot.backend.domain.place.dao
+ * FileName    : StationRepository
+ * Author      : whitecity01
+ * Date        : 26. 3. 19.
+ * Description :
+ * =====================================================================================================================
+ * DATE          AUTHOR               DESCRIPTION
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 26. 3. 19.     whitecity01       Initial creation
+ */
+public interface StationRepository extends JpaRepository<Station, Long> {
+}
