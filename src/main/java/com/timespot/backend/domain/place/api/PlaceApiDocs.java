b25286c1ac07bebf55194a696167ebe86d2e59f0
whitecity01 <kh081104@gmail.com>
2026-03-20 01:44:04 +0900

    feat: docs 정의

---
+++ src/main/java/com/timespot/backend/domain/place/api/PlaceApiDocs.java
@@ -0,0 +1,34 @@
+package com.timespot.backend.domain.place.api;
+
+import com.timespot.backend.common.response.BaseResponse;
+import com.timespot.backend.domain.place.dto.PlaceResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * PackageName : com.timespot.backend.domain.place.api
+ * FileName    : PlaceApiDocs
+ * Author      : whitecity01
+ * Date        : 26. 3. 19.
+ * Description :
+ * =====================================================================================================================
+ * DATE          AUTHOR               DESCRIPTION
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 26. 3. 19.     whitecity01       Initial creation
+ */
+@Tag(name = "Place API", description = "방문 가능 장소 조회 API")
+public interface PlaceApiDocs {
+    @Operation(summary = "방문 가능 장소 조회", description = "현재 사용자 위치에서 주어진 시간 내에 방문 후 복귀 가능한 장소를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "방문 가능 장소 조회 성공")
+    ResponseEntity<BaseResponse<List<PlaceResponseDto.AvailablePlace>>> getAvailablePlaces(
+            @RequestParam double userLat,
+            @RequestParam double userLon,
+            @RequestParam Long stationId,
+            @RequestParam int remainingMinutes
+    );
+}
