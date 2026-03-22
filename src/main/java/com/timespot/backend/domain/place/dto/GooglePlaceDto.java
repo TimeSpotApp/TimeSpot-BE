dc2a14c7034e0621a9c893082eb5b85a626b5058
whitecity01 <kh081104@gmail.com>
2026-03-22 16:51:26 +0900

    feat: 구글 api 장소 상세 정보 요청 응답 기능 구현

---
+++ src/main/java/com/timespot/backend/domain/place/dto/GooglePlaceDto.java
@@ -0,0 +1,45 @@
+package com.timespot.backend.domain.place.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import java.util.List;
+
+/**
+ * PackageName : com.timespot.backend.domain.place.dto
+ * FileName    : GooglePlaceDto
+ * Author      : whitecity01
+ * Date        : 26. 3. 22.
+ * Description :
+ * =====================================================================================================================
+ * DATE          AUTHOR               DESCRIPTION
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 26. 3. 22.     whitecity01       ADD place detail
+ */
+public class GooglePlaceDto {
+
+    @Getter
+    public static class Response {
+        private String internationalPhoneNumber;
+        private RegularOpeningHours regularOpeningHours;
+        private List<Photo> photos;
+    }
+
+    @Getter
+    public static class RegularOpeningHours {
+        private List<String> weekdayDescriptions;
+    }
+
+    @Getter
+    public static class Photo {
+        private String name;
+    }
+
+    @Getter
+    @Builder
+    public static class ParsedResult {
+        private String phoneNumber;
+        private List<String> weekdayHours;
+        private List<String> weekendHours;
+        private String imageUrl;
+    }
+}
