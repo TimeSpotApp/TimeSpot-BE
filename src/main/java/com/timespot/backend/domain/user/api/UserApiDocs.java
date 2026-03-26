0453a8bd04c3acbce5d169b24c3f4997918ca97d
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2026-03-26 13:56:59 +0900

    docs: 알림 관련 Swagger 작성

--- src/main/java/com/timespot/backend/domain/user/api/UserApiDocs.java
+++ src/main/java/com/timespot/backend/domain/user/api/UserApiDocs.java
@@ -54,6 +54,32 @@ public interface UserApiDocs {
             @Parameter(description = "회원 정보 수정 요청 페이로드") UserRequestDto.UserInfoUpdateRequest dto
     );
 
+
+    @Operation(summary = "회원 알림 설정 조회", description = "현재 사용자의 알림 시간을 조회합니다.")
+    @SecurityRequirement(name = "BearerAuth")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원 알림 설정 조회 성공"),
+            @ApiResponse(responseCode = "400",
+                    description = "유효하지 않은 인증 토큰",
+                    content = @Content(schema = @Schema(hidden = true))),
+    })
+    ResponseEntity<BaseResponse<UserResponseDto.UserNotificationResponse>> getUserNotificationSettings(
+            @Parameter(hidden = true) CustomUserDetails userDetails
+    );
+
+    @Operation(summary = "회원 알림 설정 수정", description = "현재 사용자의 알림 시간을 수정합니다.")
+    @SecurityRequirement(name = "BearerAuth")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원 알림 설정 수정 성공"),
+            @ApiResponse(responseCode = "400",
+                    description = "유효하지 않은 인증 토큰",
+                    content = @Content(schema = @Schema(hidden = true))),
+    })
+    ResponseEntity<BaseResponse<Void>> updateUserNotificationSettings(
+            @Parameter(hidden = true) CustomUserDetails userDetails,
+            @Parameter(description = "회원 알림 설정 수정 요청 페이로드") UserRequestDto.UserNotificationUpdateRequest dto
+    );
+
     @Operation(summary = "회원 탈퇴", description = "현재 사용자를 회원 탈퇴 처리합니다.")
     @SecurityRequirement(name = "BearerAuth")
     @ApiResponses({
